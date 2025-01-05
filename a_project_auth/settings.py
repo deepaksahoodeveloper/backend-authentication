1a221d61218db04be78a90c229d8050d45eaa2af
Deepak <deepaksahoodeveloper@gmail.com>
2025-01-05 17:52:25 +0530

    Add rest_framework & add b_app_auth under INSTALLED_APPS

--- a_project_auth/settings.py
+++ a_project_auth/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',  # Add DRF
+    'b_app_auth',  # Add the new app
 ]
 
 MIDDLEWARE = [
